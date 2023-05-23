5125ecede8cccfd0f37d8bac561929b98a18c96f
sametyilmaztemel <115784698+sametyilmaztemel@users.noreply.github.com>
2023-05-23 23:26:36 +0300

    Add files via upload

---
+++ Queue Dökümantasyonu.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AkademikBirikim
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueHarfÖrneği();
+            QueueTemelÖrnek();
+        }
+
+        private static void QueueHarfÖrneği()
+        {
+            // Sesli Harfleri Sırasıyla Kuyruğa Ekleyen İnteraktif Otomasyon Örneği
+
+            // Listenin tanımlanması
+
+            var sesliHarfler = new List<char>()
+            {
+                'a','e','ı','i','u','ü','o','ö'
+            };
+
+            ConsoleKeyInfo secim;
+            var kuyruk = new Queue<char>();
+
+            // Listedeki verilerin kuyruğa aktarılması
+
+            foreach (char k in sesliHarfler)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{k,-5} kuyruğa eklensin mi? [e/h]");
+                secim = Console.ReadKey();
+                Console.WriteLine();
+                if (secim.Key == ConsoleKey.E)
+                {
+                    kuyruk.Enqueue(k);
+                    Console.WriteLine($"\n{k,-5} kuyruğa eklendi.");
+                    Console.WriteLine($" Kuyruktaki eleman sayısı: {kuyruk.Count}");
+                }
+            }
+
+            // Kuyruktan eleman çıkartılması
+
+            Console.WriteLine();
+            Console.WriteLine("Kuyruktan elemanların kaldırılması işlemi için 'Esc' tuşuna basınız.");
+            secim = Console.ReadKey();
+            Console.WriteLine();
+
+            if (secim.Key == ConsoleKey.Escape)
+            {
+
+                Console.WriteLine();
+
+                while (kuyruk.Count > 0)
+                {
+                    Console.WriteLine($"{kuyruk.Peek(),5} kuyruktan çıkartılıyor.");
+                    Console.WriteLine($"{kuyruk.Dequeue(),5} kuyruktan çıkartıldı.");
+                    Console.WriteLine($" Kuyruktaki eleman sayısı: {kuyruk.Count}");
+                }
+                Console.WriteLine("\nKuyruktan çıkarma işlemi tamamlandı.");
+            }
+
+            Console.WriteLine("Program bitti.");
+
+            Console.ReadKey();
+        }
+        private static void QueueTemelÖrnek()
+        {
+            // Queue
+
+            // Tanımlama
+
+            var karakterKuyrugu = new Queue<char>(); // Sesli harfleri tutan bir kuyruk yapısı
+
+            // Ekleme
+            karakterKuyrugu.Enqueue('a');
+            karakterKuyrugu.Enqueue('e');
+            karakterKuyrugu.Enqueue('i');
+            karakterKuyrugu.Enqueue('ı');
+            karakterKuyrugu.Enqueue('u');
+            karakterKuyrugu.Enqueue('ü');
+            karakterKuyrugu.Enqueue('o');
+            karakterKuyrugu.Enqueue('ö');
+            Console.WriteLine($"Eleman sayısı {karakterKuyrugu.Count()}");
+
+            // Ek Bilgi
+
+            var dizi = karakterKuyrugu.ToArray();
+
+            // Kuyruktaki veriler doğrudan bir diziye aktarılır.
+
+            // Çıkarma
+            Console.WriteLine($"Kuyruğun başındaki eleman: {karakterKuyrugu.Peek()}");
+            Console.WriteLine($"{karakterKuyrugu.Dequeue()} kuyruktan çıkartıldı.");
+            Console.WriteLine($"Eleman sayısı: {karakterKuyrugu.Count()}");
+            Console.WriteLine($"Kuyruğun başındaki eleman: {karakterKuyrugu.Peek()}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack Dökümantasyonu.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+
+namespace AkademikBirikim
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StackSayıİşlemi();
+            StackİşlemleriÖrnek();
+            StackÖrneği();
+        }
+
+        private static void StackSayıİşlemi()
+        {
+            // Stack Yardımıyla Girilen Sayı Üzerinden İşlem Yapan Fonksiyon
+
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            var sayiYigini = new Stack<int>();
+
+            // Girilen sayıdaki basamakların tek tek yığına eklemek.
+
+            while (sayi > 0)
+            {
+                int k = sayi % 10;
+                sayiYigini.Push(k);
+                sayi = sayi / 10;
+            }
+
+            // Girilen sayı yığınının basamak sayısının hesaplamak.
+            int i = 0;
+            int n = sayiYigini.Count - 1;
+
+            foreach (var s in sayiYigini)
+            {
+                Console.WriteLine($"\t{s,7} x " +
+                    $"{Math.Pow(10, n - i),7}\t= " +
+                    $"{s * Math.Pow(10, n - i),7}");
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+        private static void StackİşlemleriÖrnek()
+        {
+            // Stack ASCII Kod Tanımlama Örneği
+
+            // Stack Tanımlama
+            var karakterYigini = new Stack<char>();
+
+            // For Döngüsü İle ASCII Karakterleri Yığına Ekleme
+            for (int i = 65; i <= 90; i++)
+            {
+                karakterYigini.Push((char)i);
+                Console.WriteLine($"{karakterYigini.Peek()} yığına eklendi.");
+                Console.WriteLine($"Yığındaki eleman sayisi : {karakterYigini.Count}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Yığından çıkartma işlemi için bir tuşa basınız.");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            // Ek Bilgi
+
+            var dizi = karakterYigini.ToArray();
+            // Yığınımızı diziye aktarmak için kullanılabilir.
+
+            // Yığından Çıkartma İşlemi
+
+            while (karakterYigini.Count > 0) // Yığında bir veri olduğu sürece işlem devam eder.
+            {
+                Console.WriteLine($"{karakterYigini.Pop()} yığından çıkartıldı.");
+                Console.WriteLine($"Yığındaki eleman sayisi : {karakterYigini.Count}");
+            }
+
+            Console.ReadKey();
+        }
+        private static void StackÖrneği()
+        {
+            // Stack Tanımlama
+            var karakterYigini = new Stack<char>();
+
+            // Stack Ekleme
+
+            karakterYigini.Push('A');
+            Console.WriteLine(karakterYigini.Peek());
+
+            karakterYigini.Push('B');
+            Console.WriteLine(karakterYigini.Peek()); // Ekleme işlemini görmemizi sağlar.
+
+            karakterYigini.Push('C');
+            Console.WriteLine(karakterYigini.Peek());
+
+            // 3 Karakter Sırasıyla Eklenmiştir En Üstte C İfadesi Bulunur.
+            // Ekleme sırasının tersine göre çıkarma işlemi yapılır.
+
+
+            // Stack Çıkarma
+
+            // Pop ifadesi çıkartma işlemini gerçekleştirirken çıkartılan karakteri çıktı olarak döndürür.
+            // Bu sayede cw yardımıyla çıkartılan karakteri ekrana yazdırabiliriz.
+
+            Console.WriteLine(karakterYigini.Pop() + " yığından çıkartıldı.");
+            Console.WriteLine(karakterYigini.Pop() + " yığından çıkartıldı.");
+            Console.WriteLine(karakterYigini.Pop() + " yığından çıkartıldı.");
+
+
+            Console.ReadKey();
+        }
+    }
+}
